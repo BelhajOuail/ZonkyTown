583db80e91b49c4e305ce6889cd911bf5037efd0
denvenum1 <147246499+denvenum1@users.noreply.github.com>
2024-04-30 09:05:06 +0000

    middleware

--- ZonkyTown/index.ts
+++ ZonkyTown/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "./public/router/fortnite";
 import { connect } from './mongoDB';
+import { errorHandler } from "./public/middleware/middleware"
 
 const app = express();
 
@@ -12,6 +13,13 @@ app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router); 
+
+app.use(errorHandler(404, "The page you were trying to find does not exists"))
+   .use(errorHandler(500, "Internal server error. Please try again later."))
+   .use(errorHandler(403, "Forbidden. Access denied."))
+   .use(errorHandler(401, "Unauthorized. Please log in."))
+   .use(errorHandler(400, "Bad request. Invalid syntax."));
+
 app.listen(app.get("port"), async () => {
     connect();
     console.log("server http://localhost:" + app.get("port"));
